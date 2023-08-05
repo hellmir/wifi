2b19430e66f5a8219d1d7ca300d2644120ee068b
Sung Hyobin <hellmir610@gmail.com>
2023-08-05 23:29:32 +0900

    Initial commit

---
+++ src/test/java/personal/WifiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.wifi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WifiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
