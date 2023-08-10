4e611d82e60e8dc1b7be5ec134d5b37dca4e2b3b
Sung Hyobin <hellmir610@gmail.com>
2023-08-10 22:45:46 +0900

    feat(Function): 거리 계산 후 주변 와이파이 정보 20개 반환 기능 추가
    JPA 적용

--- src/main/java/personal/wifi/repository/wifi/WifiRepository.java
+++ src/main/java/personal/wifi/repository/wifi/WifiRepository.java
@@ -1,79 +1,39 @@
 package personal.wifi.repository.wifi;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import personal.wifi.entity.wifi.WifiData;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+import java.util.List;
 
-@Repository
-public class WifiRepository {
+public interface WifiRepository extends JpaRepository<WifiData, Long> {
 
-    public void createWifiTable() {
+    @Query(value = "SELECT w.X_SWIFI_MGR_NO," +
+            "w.X_SWIFI_WRDOFC," +
+            "w.X_SWIFI_MAIN_NM," +
+            "w.X_SWIFI_ADRES1," +
+            "w.X_SWIFI_ADRES2," +
+            "w.X_SWIFI_INSTL_FLOOR," +
+            "w.X_SWIFI_INSTL_TY," +
+            "w.X_SWIFI_INSTL_MBY," +
+            "w.X_SWIFI_SVC_SE," +
+            "w.X_SWIFI_CMCWR," +
+            "w.X_SWIFI_CNSTC_YEAR," +
+            "w.X_SWIFI_INOUT_DOOR," +
+            "w.X_SWIFI_REMARS3," +
+            "w.LAT," +
+            "w.LNT," +
+            "w.WORK_DTTM, " +
+            "(6371 * acos(cos(radians(:latitude)) " +
+            "* cos(radians(w.LNT)) " +
+            "* cos(radians(w.LAT) - radians(:longitude)) " +
+            "+ sin(radians(:latitude)) " +
+            "* sin(radians(w.LNT)))) AS distance " +
+            "FROM WIFI_DATA w " +
+            "ORDER BY distance " +
+            "LIMIT 20", nativeQuery = true)
+    List<WifiData> findNearTwentyWifis(@Param("latitude") double latitude, @Param("longitude") double longitude);
 
-        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db");
-             Statement stmt = c.createStatement()) {
-
-
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-        }
-
-        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db");
-             Statement stmt = c.createStatement()) {
-
-            stmt.execute("DROP TABLE IF EXISTS WIFI_TABLE");
-
-            String sql = "CREATE TABLE WIFI_TABLE " +
-                    "(X_SWIFI_MGR_NO TEXT PRIMARY KEY NOT NULL, " +
-                    "X_SWIFI_WRDOFC TEXT NOT NULL, " +
-                    "X_SWIFI_MAIN_NM TEXT NOT NULL, " +
-                    "X_SWIFI_ADRES1 TEXT NOT NULL, " +
-                    "X_SWIFI_ADRES2 TEXT NOT NULL, " +
-                    "X_SWIFI_INSTL_FLOOR TEXT NOT NULL, " +
-                    "X_SWIFI_INSTL_TY TEXT NOT NULL, " +
-                    "X_SWIFI_INSTL_MBY TEXT NOT NULL, " +
-                    "X_SWIFI_SVC_SE TEXT NOT NULL, " +
-                    "X_SWIFI_CMCWR TEXT NOT NULL, " +
-                    "X_SWIFI_CNSTC_YEAR TEXT NOT NULL, " +
-                    "X_SWIFI_INOUT_DOOR TEXT NOT NULL, " +
-                    "X_SWIFI_REMARS3 TEXT NOT NULL, " +
-                    "LAT TEXT NOT NULL, " +
-                    "LNT TEXT NOT NULL, " +
-                    "WORK_DTTM TEXT NOT NULL)";
-            stmt.executeUpdate(sql);
-
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-        }
-
-    }
-
-    public void save(WifiData wifiInformation) {
-
-        createWifiTable();
-
-        try (Connection c = DriverManager.getConnection("jdbc:sqlite:test.db");
-             Statement stmt = c.createStatement()) {
-
-            String sql = "INSERT INTO WIFI_TABLE (X_SWIFI_MGR_NO, X_SWIFI_WRDOFC, X_SWIFI_MAIN_NM, X_SWIFI_ADRES1," +
-                    "X_SWIFI_ADRES2, X_SWIFI_INSTL_FLOOR, X_SWIFI_INSTL_TY, X_SWIFI_INSTL_MBY, X_SWIFI_SVC_SE," +
-                    "X_SWIFI_CMCWR, X_SWIFI_CNSTC_YEAR, X_SWIFI_INOUT_DOOR, X_SWIFI_REMARS3, LAT, LNT, WORK_DTTM) " +
-                    "VALUES ('" + wifiInformation.getManagementNo() + "', '" + wifiInformation.getWardOffice()
-                    + "', '" + wifiInformation.getMainName() + "', '" + wifiInformation.getAddress1()
-                    + "', '" + wifiInformation.getAddress2() + "', '" + wifiInformation.getInstallationFloor()
-                    + "', '" + wifiInformation.getInstallationType() + "', '" + wifiInformation.getInstallationManufacturedBy()
-                    + "', '" + wifiInformation.getServiceSeparatedEntry() + "', '" + wifiInformation.getCMCWR()
-                    + "', '" + wifiInformation.getConstructionYear() + "', '" + wifiInformation.getInoutDoor()
-                    + "', '" + wifiInformation.getREMARS3() + "', '" + wifiInformation.getLatitude()
-                    + "', '" + wifiInformation.getLongitude() + "', '" + wifiInformation.getWorkedDateTime() + "')";
-            stmt.executeUpdate(sql);
-
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
-        }
-
-    }
 
 }
