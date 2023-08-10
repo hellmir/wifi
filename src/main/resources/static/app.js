eda7c5c4766c4c5b214db3adf37eb7607c90a16e
Sung Hyobin <hellmir610@gmail.com>
2023-08-10 22:49:02 +0900

    chore(Line Breaks): app.js의 개행 수정

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -27,10 +27,42 @@ document.getElementById('loadWifi').addEventListener
 
     event.preventDefault();
 
+<<<<<<< Updated upstream
     $.post("/wifis", function (wifiCount) {
         document.getElementById('result').textContent = wifiCount + "개의 WIFI 정보를 정상적으로 저장하였습니다.";
     }).fail(function () {
         document.getElementById('result').textContent = "와이파이 정보를 불러오는 데 실패했습니다.";
+=======
+        if (wifiDataResponseDtoList.length === 0) {
+            tableBodyHtml = '<tr><td colspan="17">위치 정보를 입력한 후에 조회해 주세요.</td></tr>';
+        } else {
+            wifiDataResponseDtoList.forEach(function (wifi) {
+
+                tableBodyHtml += '<tr>' +
+                    '<td>' + wifi.distance + '</td>' +
+                    '<td>'+ wifi.managementNo + '</td>' +
+                    '<td>' + wifi.wardOffice + '</td>' +
+                    '<td>' + wifi.mainName + '</td>' +
+                    '<td>' + wifi.address1 + '</td>' +
+                    '<td>' + wifi.address2 + '</td>' +
+                    '<td>' + wifi.installationFloor + '</td>' +
+                    '<td>' + wifi.installationType + '</td>' +
+                    '<td>' + wifi.installationManufacturedBy + '</td>' +
+                    '<td>' + wifi.serviceSeparatedEntry + '</td>' +
+                    '<td>' + wifi.CMCWR + '</td>' +
+                    '<td>' + wifi.constructionYear + '</td>' +
+                    '<td>' + wifi.inoutDoor + '</td>' +
+                    '<td>' + wifi.REMARS3 + '</td>' +
+                    '<td>' + wifi.latitude + '</td>' +
+                    '<td>' + wifi.longitude + '</td>' +
+                    '<td>' + wifi.workedDateTime + '</td>' +
+                    '</tr>';
+                
+            });
+        }
+
+        document.getElementById('wifiTableBody').innerHTML = tableBodyHtml;
+>>>>>>> Stashed changes
     });
 
 });
